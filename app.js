70ff431a6c82b0f4ae9d51c64fde29b304e54cd5
Victor Immanuel <chrollolucilfer1402@gmail.com>
2022-12-29 21:38:34 +0530

    added log-in and sign-up part that appears on clicking account button and disappears on clicking cross button

--- app.js
+++ app.js
@@ -24,3 +24,13 @@ function func2(){
     document.querySelector('.signup').style.display="initial";
     document.querySelector(".log-in").style.display="none";
 }
+
+// Function to display log-in section on click
+
+function userAuth(){
+    document.querySelector('.user-signup').style.display="block";
+}
+
+function loginClose(){
+    document.querySelector('.user-signup').style.display="none";
+}
