e5dda35993188a2966de30e5fe1f1d08e54b2a70
Victor Immanuel <chrollolucilfer1402@gmail.com>
2023-03-11 21:57:20 +0530

    add responsive header to product-page

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,26 @@
 module.exports = {
   content: ["./HTML/*.{html, js}"],
   theme: {
+    fontFamily: {
+      mono: ['Calibre', 'Inter', 'San Francisco', 'SF Pro Text', "-apple-system", "system-ui", "sans-serif"],
+      sans: ['SF Mono', 'Fira Code', 'Fira Mono', 'Roboto Mono', 'monospace'],
+      avenirNext: ["Georgia","Times",'Times New Roman',"serif"],
+    },
+    colors:{
+      green: "#64ffda",
+      Navy: "#0a192f",
+      lighest_Navy: "#233554",
+      light_Navy: "#112240",
+      slate: "#8892B0",
+      light_Slate: "#a8b2d1",
+      lightest_Slate: "#ccd6f6",
+      white: "white",
+      black: "black",
+      red: "red",
+      blue: "blue",
+      yellow: "yellow",
+      transparent: "transparent"
+    },
     extend: {},
   },
   plugins: [],
