a8c24291f8bc55d81ab9cca6e134c7670efb7623
Victor Immanuel <chrollolucilfer1402@gmail.com>
2023-03-14 20:13:03 +0530

    done with adding js to make the aside fixed when it reaches header and normal when scrolled up

--- JavaScript/product.js
+++ JavaScript/product.js
@@ -5,6 +5,7 @@ const login = document.querySelector('.login');
 const register = document.querySelector('.register');
 const loginForm = document.querySelector('.login-form');
 const signUpForm = document.querySelector('.register-form');
+const productSection = document.querySelector('.products-options-list')
 const productsOption = document.querySelector('.collections-name');
 const productList = document.querySelector(".product-list-items") ;
 
@@ -40,32 +41,44 @@ const showLogin = (e) => {
 }
 
 // making the product options fixed
-function makeCollectionsFixed(){
-        const topDistance = productsOption.getBoundingClientRect().top;
-        console.log(topDistance);
-        if(topDistance <= 64){
-            let asideList = ["fixed", "top-16"];
+function makeCollectionsFixed(e){
+        let asideList = ["fixed", "top-16"];
+        const topDistance = productSection.getBoundingClientRect().top;
+        const optionWidth = productsOption.clientWidth;
+        console.log(optionWidth);
+        if(!hasRun && topDistance <= 64){
+            console.log(topDistance);
             for (let className of asideList){
                 console.log(className);
                 productsOption.classList.add(className);
             }
             console.log(productsOption);
-    
-            productList.classList.add("ml-56");
-            window.removeEventListener("scroll", makeCollectionsFixed);
+            
+            productList.style.margin = `0 0 0 ${optionWidth+5}px`;
+            hasRun = true;
         }
+        // console.log(topDistance)
+        if(topDistance > 64){
+            for (let className of asideList){
+                productsOption.classList.remove(className);
+            }
+            productList.style.margin = "0";
+            hasRun = false;
+        }
+
+
     
 }
 
 
-
+// Execution
 account.addEventListener("click", showLog);
 cancel.addEventListener("click", hideLog);
 
 login.addEventListener("click", showLogin);
 register.addEventListener("click", showLogin);
 
-
+let hasRun = false;
 window.addEventListener('scroll', makeCollectionsFixed);
 
 
