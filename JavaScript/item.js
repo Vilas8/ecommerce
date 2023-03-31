cd97d1f45a5f4dc30f4e666859aa252bed578d93
Victor Immanuel <chrollolucilfer1402@gmail.com>
2023-03-31 13:19:24 +0530

    made the carousal move automatically + prev/next button functionality

--- JavaScript/item.js
+++ JavaScript/item.js
@@ -1,34 +1,52 @@
-let slideIndex = 0;
-let slides = document.querySelectorAll('.slide');
+const carousal = document.querySelector(".carousel");
 const prev = document.querySelector(".prev");
 const next = document.querySelector(".next");
 
+let timer;
+let slideIndex = 0;
+let slides = document.querySelectorAll('.slide');
+
+
 const showSlides = () =>{
-  let i = slideIndex;
-  for (i; i <= slides.length; i++){
-    if (i == 0){
-      slides[i].classList.add('active');
-      break;
+  let i;
+  for (i = 0; i <  slides.length; i++){
+    if(slides[i].classList.contains("active")){
+        slides[i].classList.remove("active");
     }
-    else if (i > 0 && i <= slides.length - 1){
-      slides[i].classList.add("active");
-      slides[i-1].classList.remove("active");
-      break;
-    }
-    else {
-      slides[i-1].classList.remove("active");
-      slides[0].classList.add("active");
-      slideIndex = 0;
-      break;
-    }
-    
+  
   }
+  if(slideIndex === slides.length){
+    slideIndex = 0;
+  }
+  slides[slideIndex].classList.add("active");
+  
   console.log(slideIndex);
   slideIndex++;
-  setTimeout(showSlides, 5000);
+}
+
+const prevSlide = () => {
+    slideIndex -= 2;
+    if(slideIndex < 0){
+      slideIndex = slides.length - 1;
+    }
+    showSlides();
+}
+
+const nextSlide = () => {
+    showSlides();
 }
 
 
 // Execution
-slides[slideIndex].classList.add("active");
 showSlides();
+timer  = setInterval(showSlides, 3000);
+carousal.addEventListener("mouseenter", () => {
+  clearInterval(timer);
+})
+
+carousal.addEventListener("mouseleave", () => {
+  timer = setInterval(showSlides, 3000)
+})
+
+prev.addEventListener("click", prevSlide);
+next.addEventListener("click", nextSlide);
